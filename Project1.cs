69b2fb58edd613e9d59a31da2793640a1990d53b
ILKHOM <gaffarovilkhom@inbox.ru>
2023-11-29 00:35:53 +0300

    Добавьте файлы проекта.

---
+++ Project1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+class Project1
+{
+	static void Min(in int[][] array, out string result)
+	{
+		result = "";
+		for (int i = 0; i < array.Length; i++)
+		{
+			int min = array[i][0];
+			for (int j = 1; j < array[i].Length; j++)
+			{
+				if (array[i][j] < min)
+					min = array[i][j];
+			}
+			result += $"\nСтрока {i + 1}: {min} ";
+		}
+	}
+
+	static void Max(in int[][] array, out string result)
+	{
+		result = "";
+		for (int i = 0; i < array.Length; i++)
+		{
+			int max = array[i][0];
+			for (int j = 1; j < array[i].Length; j++)
+			{
+				if (array[i][j] > max)
+					max = array[i][j];
+			}
+			result += $"\nСтрока {i + 1}: {max} ";
+		}
+	}
+
+	static void Sum(in int[][] array, ref int[] result)
+	{
+		int pointer = 0;
+		for (int i = 0; i < array.Length; i++)
+		{
+			int sum = 0;
+			for (int j = 0; j < array[i].Length; j++)
+				sum += array[i][j];
+			result[pointer++] = sum;
+		}
+	}
+	static void Main()
+	{
+		int n = 0;
+		do
+		{
+			Console.Write("Введите количество строк: ");
+			if (int.TryParse(Console.ReadLine(), out n))
+			{
+				if (n > 0)
+				{
+					break;
+				}
+			}
+			Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+		} while (true);
+
+		int pointer = 0;
+		int[][] array = new int[n][];
+
+
+		for (int i = 0; i < n; i++)
+		{
+			int counter = 0;
+			string user_input = Console.ReadLine();
+			string line = user_input[0].ToString();
+
+			for (int j = 1; j < user_input.Length; j++)
+			{
+				if (user_input[j] != ' ' && user_input[j - 1] != ' ' || user_input[j] != ' ' && user_input[j - 1] == ' ')
+				{
+					line += user_input[j];
+					if (j == user_input.Length - 1)
+						counter++;
+					continue;
+				}
+				else if (user_input[j] == ' ' && user_input[j - 1] == ' ')
+				{
+					continue;
+				}
+				else if (user_input[j] == ' ' && user_input[j - 1] != ' ')
+				{
+					counter++;
+					line = "";
+				}
+			}
+
+			array[pointer++] = new int[counter];
+			line = user_input[0].ToString();
+			int array_index = 0;
+			for (int j = 1; j < user_input.Length; j++)
+			{
+				if (user_input[j] != ' ' && user_input[j - 1] != ' ' || user_input[j] != ' ' && user_input[j - 1] == ' ')
+				{
+					line += user_input[j];
+					int number = 0;
+					if (j == user_input.Length - 1)
+					{
+						if (int.TryParse(line, out number))
+						{
+							array[i][array_index++] = number;
+						}
+						else
+						{
+							number = 0;
+							array[i][array_index++] = number;
+						}
+						continue;
+					}
+				}
+				else if (user_input[j] == ' ' && user_input[j - 1] == ' ')
+				{
+					continue;
+				}
+				else if (user_input[j] == ' ' && user_input[j - 1] != ' ')
+				{
+					int number = 0;
+					if (int.TryParse(line, out number))
+					{
+						array[i][array_index++] = number;
+					}
+					else
+					{
+						number = 0;
+						array[i][array_index++] = number;
+					}
+					line = "";
+				}
+			}
+		}
+
+		Min(in array, out string result_1);
+		Console.WriteLine($"Минимумы: {result_1}");
+
+		Max(in array, out string result_2);
+		Console.WriteLine($"Максимумы: {result_2}");
+
+		int[] result_3 = new int[n];
+		Sum(in array, ref result_3);
+		Console.Write("Суммы: ");
+		for (int i = 0; i < n; i++)
+			Console.Write($"\nСтрока {i + 1}: {result_3[i]} ");
+	}
+}
---
+++ Project3.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+class Project3
+{
+	private int[] array;
+	private int size;
+
+	public Project3(int n)
+	{
+		size = n;
+		array = new int[n];
+	}
+
+	public void InputData()
+	{
+		Console.WriteLine("Введите значения массива: ");
+		for (int i = 0; i < size; i++)
+		{
+			if (int.TryParse(Console.ReadLine(), out int number))
+			{
+				array[i] = number;
+			}
+			else
+			{
+				i--;
+			}
+		}
+	}
+
+	public void InputDataRandom()
+	{
+		Console.WriteLine("Случайное задание числами от 0 до 10");
+		Random random = new Random();
+		for (int i = 0; i < size; i++)
+			array[i] = random.Next(0, 10);
+	}
+
+	public void Print()
+	{
+		Console.Write("Массив: ");
+		for (int i = 0; i < size; i++)
+			Console.Write($"{array[i]} ");
+		Console.WriteLine();
+	}
+
+	public string FindValue(in int number)
+	{
+		string result = "Индексы: ";
+		int counter = 0;
+		for (int i = 0; i < size; i++)
+		{
+			if (array[i] == number)
+			{
+				counter++;
+				result += $"{i + 1} ";
+			}
+		}
+		return result;
+	}
+
+	public void DelValue(int number)
+	{
+		int counter = 0;
+		for (int i = 0; i < size; i++)
+		{
+			if (array[i] == number)
+				counter++;
+		}
+		if (counter == 0)
+		{
+			Console.WriteLine("Искомый элемент не найден");
+			return;
+		}
+		int[] new_array = new int[size - counter];
+		int index = 0;
+		for (int i = 0; i < size; i++)
+		{
+			if (array[i] == number)
+				continue;
+			new_array[index++] = array[i];
+		}
+		array = new_array;
+		size -= counter;
+	}
+
+	public void FindMax(out int result)
+	{
+		result = 0;
+		foreach (int i in array)
+		{
+			if (i > result)
+				result = i;
+		}
+	}
+
+	public static void Sort(ref Project3 array)
+	{
+		for (int i = 0; i < array.size; i++)
+		{
+			for (int j = i + 1; j < array.size; j++)
+			{
+				if (array.array[i] > array.array[j])
+				{
+					int temp = array.array[i];
+					array.array[i] = array.array[j];
+					array.array[j] = temp;
+				}
+			}
+		}
+	}
+}
+
+public class Program3
+{
+	static void Main()
+	{
+		Project3 array = new Project3(10);
+		array.InputDataRandom();
+		array.Print();
+		Project3.Sort(ref array);
+		array.Print();
+		array.FindMax(out int result);
+		Console.WriteLine($"Макс. элемент: {result}");
+		array.DelValue(result);
+		array.Print();
+		Console.Write("Введите искомое значение: ");
+		int number = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine(array.FindValue(in number));
+	}
+}
